e19acf489d1b46b58ede5ecc4cfdbc6f63080cc6
sakamotosyo <syo10150000@gmail.com>
2022-09-13 15:26:11 +0900

    バグの修正

--- Assets/okabe/Script/P2komaContlrol.cs
+++ Assets/okabe/Script/P2komaContlrol.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class P2komaContlrol : MonoBehaviour
 {
     [SerializeField] Rigidbody2D _Rigidbody2D;
+    [SerializeField] Generator _generator;
     bool _Gravity;
     bool _Ground;
+    bool _ins;
+    float _time = 0;
 
     private void Start()
     {
@@ -17,52 +20,54 @@ public class P2komaContlrol : MonoBehaviour
 
     void Update()
     {
-       
-            if (!_Gravity)
+
+        if (!_Gravity)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad4))
             {
-                if (Input.GetKeyDown(KeyCode.Keypad4))
-                {
-                    transform.position = transform.position + new Vector3(-0.1f, 0, 0);
-                }
-                if (Input.GetKeyDown(KeyCode.Keypad6))
-                {
-                    transform.position = transform.position + new Vector3(0.1f, 0, 0);
-                }
-                if (Input.GetKeyDown(KeyCode.Keypad7))
-                {
-                    transform.Rotate(0, 0, 30);
-                }
-                if (Input.GetKeyDown(KeyCode.Keypad9))
-                {
-                    transform.Rotate(0, 0, -30);
-                }
-                if (Input.GetKeyDown(KeyCode.KeypadEnter))
-                {
-                    _Rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+                transform.position = transform.position + new Vector3(-0.1f, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad6))
+            {
+                transform.position = transform.position + new Vector3(0.1f, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad7))
+            {
+                transform.Rotate(0, 0, 30);
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad9))
+            {
+                transform.Rotate(0, 0, -30);
+            }
+            if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                _Rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
                 _Gravity = true;
 
             }
         }
-      
-            else if(_Ground)
+
+        else if (_Ground && !_ins && _Gravity)
+        {
+            float x = gameObject.transform.position.x;
+            float y = gameObject.transform.position.y;
+            if (_Rigidbody2D.velocity == new Vector2(0, 0))
             {
-                float x = gameObject.transform.position.x;
-                float y = gameObject.transform.position.y;
-                if (x < 0.1f && y < 0.1f)
-                {
-                    Debug.Log("‚¤");
-              
-                    // Instantiate
-                }
+                Debug.Log("‚¤");
+
+                _generator.ItemGenerator();
+                _ins = true;
+
             }
-        
+        }
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Stage")/*|| collision.gameObject.CompareTag("‹î‚Ìƒ^ƒO") */ )
+        if (collision.gameObject.CompareTag("Stage"))
         {
             _Ground = true;
-        
+
         }
     }
 }
